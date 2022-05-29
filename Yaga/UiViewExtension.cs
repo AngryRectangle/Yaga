b5d14e72a0acfddc9fe1f8286d5d37b7f10a7902
AngryRectangle <yashmel2000@gmail.com>
2022-05-29 13:52:00 +0300

    docs: add docs to buttons subscriptions

--- Yaga/UiViewExtension.cs
+++ Yaga/UiViewExtension.cs
@@ -6,12 +6,18 @@ namespace Yaga
 {
     public static class UiViewExtension
     {
+        /// <summary>
+        /// Subscribe on button click and dispose subscription when needed.
+        /// </summary>
         public static void Subscribe(this View view, Button button, UnityAction action)
         {
             button.onClick.AddListener(action);
             view._disposables.Add(new Reflector(() => button.onClick.RemoveListener(action)));
         }
         
+        /// <summary>
+        /// Subscribe on button click, execute action and dispose subscription when needed.
+        /// </summary>
         public static void SubscribeAndCall(this View view, Button button, UnityAction action)
         {
             button.onClick.AddListener(action);
