910bf30bb6d7de8c5bc6d5dec30c3251cb674182
AngryRectangle <yashmel2000@gmail.com>
2022-06-05 00:22:12 +0300

    test: add modeless view testing

---
+++ Yaga.Test/BaseUiTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+
+namespace Yaga.Test
+{
+    public class BaseUiTest
+    {
+        private PrefabLocator _locator;
+
+        public PrefabLocator Locator
+        {
+            get
+            {
+                if(_locator is null)
+                    _locator = AssetDatabase.LoadAssetAtPath<PrefabLocator>("Assets/Yaga.Test/TestPrefabLocator.asset");
+                return _locator;
+            }
+        }
+        
+        [SetUp]
+        public void SetUp()
+        {
+            UiBootstrap.InitializeSingleton();
+            UiControl.InitializeSingleton(Locator.canvasPrefab);
+        }
+    }
+}
---
+++ Yaga.Test/ModelessViewTest.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Yaga.Test
+{
+    public class ModelessViewTest : BaseUiTest
+    {
+        [UnityTest]
+        public IEnumerator ModelessViewSet()
+        {
+            UiBootstrap.Bind<PresenterSetTest>();
+            UiControl.Instance.Create(Locator.modelessView);
+            yield return null;
+            Assert.Fail("OnSet method was not called");
+        }
+
+        public class PresenterSetTest : Presenter<ModelessView>
+        {
+            protected override void OnSet(ModelessView view)
+            {
+                Assert.Pass();
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ModelessViewUnset()
+        {
+            UiBootstrap.Bind(new PresenterUnsetTest(Assert.Pass));
+            var instance = UiControl.Instance.Create(Locator.modelessView);
+            yield return null;
+            UiBootstrap.Instance.Unset(instance);
+            Assert.Fail("OnUnset method was not called");
+        }
+
+        [UnityTest]
+        public IEnumerator ModelessViewUnsetOnDestroy()
+        {
+            var unSetWasCalled = false;
+            UiBootstrap.Bind(new PresenterUnsetTest(() => unSetWasCalled = true));
+            var instance = UiControl.Instance.Create(Locator.modelessView);
+            yield return null;
+            GameObject.Destroy(instance);
+            yield return null;
+            Assert.True(unSetWasCalled, "OnUnset method was not called");
+        }
+
+        public class PresenterUnsetTest : Presenter<ModelessView>
+        {
+            private readonly Action _onUnset;
+
+            public PresenterUnsetTest(Action onUnset)
+            {
+                _onUnset = onUnset;
+            }
+
+            protected override void OnUnset(ModelessView view)
+            {
+                _onUnset();
+            }
+        }
+    }
+}
