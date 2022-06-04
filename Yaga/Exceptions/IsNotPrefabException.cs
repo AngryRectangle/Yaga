98b744023fc71d56889a730c09edd6182de03aae
AngryRectangle <yashmel2000@gmail.com>
2022-06-04 22:48:22 +0300

    refactor: add more exceptions and checks to UiControl.cs

---
+++ Yaga/Exceptions/IsNotPrefabException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Yaga.Exceptions
+{
+    /// <summary>
+    /// Thrown when regular game object is being used as prefab.
+    /// </summary>
+    public class IsNotPrefabException : Exception
+    {
+        public GameObject GameObject { get; }
+
+        public IsNotPrefabException(GameObject gameObject) : base($"Game object {gameObject} is not prefab.")
+        {
+            GameObject = gameObject;
+        }
+    }
+}
