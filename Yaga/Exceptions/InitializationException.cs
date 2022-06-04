754a5f2597747578ed259008d33a9d91a7496c47
AngryRectangle <yashmel2000@gmail.com>
2022-06-04 21:57:20 +0300

    refactor: add more exceptions and checks to UiBootstrap.cs

---
+++ Yaga/Exceptions/InitializationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Yaga.Exceptions
+{
+    public class InitializationException : Exception
+    {
+        public InitializationException()
+        {
+        }
+
+        public InitializationException(string message) : base(message)
+        {
+        }
+
+        public InitializationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
