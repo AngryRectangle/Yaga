e54c346fa10539d6afab1e72339a63ac32ee2b47
AngryRectangle <yashmel2000@gmail.com>
2024-02-03 20:45:08 +0100

    DDDD

--- Tests/BaseUiTest.cs
+++ Tests/BaseUiTest.cs
@@ -18,8 +18,8 @@ namespace Tests
         private T GetFirstWithName<T>(string name)
             where T : Object
         {
-            var files = Directory.GetFiles(Application.dataPath, name, SearchOption.AllDirectories);
-            throw new Exception(string.Join("\n", files));
+            var files = Directory.GetFiles(Application.dataPath, name, SearchOption.TopDirectoryOnly);
+            throw new Exception(Application.dataPath + " " +string.Join("\n", files));
             foreach (var file in files.Select(file => file.Replace("\\", "/")
                          .Remove(0, Application.dataPath.Length - Application.dataPath.Split('/').Last().Length)))
             {
