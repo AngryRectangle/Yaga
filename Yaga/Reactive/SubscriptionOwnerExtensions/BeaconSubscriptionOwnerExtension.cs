a1a199768f89d182e0b466721b0be8aefcecdfa7
AngryRectangle <yashmel2000@gmail.com>
2024-02-03 13:53:21 +0100

    refactor: split ObservableSubscriptionOwnerExtension into new classes

---
+++ Yaga/Reactive/SubscriptionOwnerExtensions/BeaconSubscriptionOwnerExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Yaga.Reactive
+{
+    public static class BeaconSubscriptionOwnerExtension
+    {
+        /// <summary>
+        /// Subscribe on beacon and dispose subscription when needed.
+        /// </summary>
+        public static void Subscribe(this ISubscriptionsOwner owner, Beacon observable, Action action)
+            => owner.Add(observable.Add(action));
+
+        /// <inheritdoc cref="Subscribe(ISubscriptionsOwner,Beacon,Action)"/>
+        public static void Subscribe<T>(this ISubscriptionsOwner owner, Beacon<T> observable, Action<T> action)
+            => owner.Add(observable.Add(action));
+
+        /// <inheritdoc cref="Subscribe(ISubscriptionsOwner,Beacon,Action)"/>
+        public static void Subscribe<T1, T2>(this ISubscriptionsOwner owner, Beacon<T1, T2> observable,
+            Action<T1, T2> action)
+            => owner.Add(observable.Add(action));
+
+        /// <inheritdoc cref="Subscribe(ISubscriptionsOwner,Beacon,Action)"/>
+        public static void Subscribe<T1, T2, T3>(this ISubscriptionsOwner owner, Beacon<T1, T2, T3> observable,
+            Action<T1, T2, T3> action)
+            => owner.Add(observable.Add(action));
+    }
+}
--- Yaga/Reactive/SubscriptionOwnerExtensions/ObservableSubscriptionOwnerExtension.cs
+++ Yaga/Reactive/SubscriptionOwnerExtensions/ObservableSubscriptionOwnerExtension.cs
@@ -6,26 +6,6 @@ namespace Yaga.Reactive
     public static class ObservableSubscriptionOwnerExtension
     {
         /// <summary>
-        /// Subscribe on beacon and dispose subscription when needed.
-        /// </summary>
-        public static void Subscribe<T1, T2>(this ISubscriptionsOwner owner, Beacon<T1, T2> observable,
-            Action<T1, T2> action)
-            => owner.Add(observable.Add(action));
-
-        /// <summary>
-        /// Subscribe on beacon and dispose subscription when needed.
-        /// </summary>
-        public static void Subscribe<T1, T2, T3>(this ISubscriptionsOwner owner, Beacon<T1, T2, T3> observable,
-            Action<T1, T2, T3> action)
-            => owner.Add(observable.Add(action));
-
-        /// <summary>
-        /// Subscribe on beacon and dispose subscription when needed.
-        /// </summary>
-        public static void Subscribe<T>(this ISubscriptionsOwner owner, Beacon<T> observable, Action<T> action)
-            => owner.Add(observable.Add(action));
-
-        /// <summary>
         /// Subscribe on observable value change, execute action and dispose subscription when needed.
         /// </summary>
         public static void SubscribeAndCall<T>(this ISubscriptionsOwner owner, IObservable<T> observable,
@@ -51,12 +31,6 @@ namespace Yaga.Reactive
         public static void Subscribe<T>(this ISubscriptionsOwner owner, IOptionalObservable<T> observable,
             Action<T> action, Action onNull)
             => owner.Add(observable.Subscribe(action, onNull));
-
-        /// <summary>
-        /// Subscribe on beacon and dispose subscription when needed.
-        /// </summary>
-        public static void Subscribe(this ISubscriptionsOwner owner, Beacon observable, Action action)
-            => owner.Add(observable.Add(action));
         
         /// <summary>
         /// Subscribe on observable value change and dispose subscription when needed.
