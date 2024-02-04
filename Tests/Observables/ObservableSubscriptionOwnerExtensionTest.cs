b167518a59341c658f3c9b7104cb13626d7834a3
AngryRectangle <yashmel2000@gmail.com>
2024-02-04 22:08:58 +0100

    feat: add set extension method for OptionalObservable

--- Tests/Observables/ObservableSubscriptionOwnerExtensionTest.cs
+++ Tests/Observables/ObservableSubscriptionOwnerExtensionTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NUnit.Framework;
+using Optional;
 using Tests.Presenters;
 using Yaga;
 using Yaga.Reactive;
@@ -18,108 +19,341 @@ namespace Tests.Observables
         }
 
         [Test]
-        public void Set_InitialValue()
+        public void Set_Observable_InitialValue()
         {
             var observableText = new Observable<string>("initial");
             var presenter = new ChildrenViewPresenter(observableText);
             UiBootstrap.Instance.Bind(presenter);
             UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
             UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
-            
+
             var view = UiControl.Instance.Create(Locator.viewWithChild);
-            
+
             Assert.AreEqual("initial", view.View.viewWithModel.Text.text);
         }
-        
+
         [Test]
-        public void Set_InitialValue_ParentUnset()
+        public void Set_Observable_InitialValue_ParentUnset()
         {
             var observableText = new Observable<string>("initial");
             var presenter = new ChildrenViewPresenter(observableText);
             UiBootstrap.Instance.Bind(presenter);
             UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
             UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
-            
+
             var view = UiControl.Instance.Create(Locator.viewWithChild);
             view.Unset();
             observableText.Value = "changed";
-            
-            // Unsubscribed, so the value should not change
-            Assert.AreEqual("initial", view.View.viewWithModel.Text.text);
-        }
-        
-        [Test]
-        public void Set_InitialValue_Unsubscribed()
-        {
-            var observableText = new Observable<string>("initial");
-            var presenter = new ChildrenViewPresenter(observableText);
-            UiBootstrap.Instance.Bind(presenter);
-            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
-            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
-            
-            var view = UiControl.Instance.Create(Locator.viewWithChild);
-            presenter.unsubscriptionKey.Dispose();
-            observableText.Value = "changed";
-            
+
             // Unsubscribed, so the value should not change
             Assert.AreEqual("initial", view.View.viewWithModel.Text.text);
         }
 
         [Test]
-        public void Set_ChangeValue()
+        public void Set_Observable_InitialValue_Unsubscribed()
         {
             var observableText = new Observable<string>("initial");
             var presenter = new ChildrenViewPresenter(observableText);
             UiBootstrap.Instance.Bind(presenter);
             UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
             UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
-            
+
             var view = UiControl.Instance.Create(Locator.viewWithChild);
-            
+            presenter.unsubscription.Dispose();
+            observableText.Value = "changed";
+
+            // Unsubscribed, so the value should not change
+            Assert.AreEqual("initial", view.View.viewWithModel.Text.text);
+        }
+
+        [Test]
+        public void Set_Observable_ChangeValue()
+        {
+            var observableText = new Observable<string>("initial");
+            var presenter = new ChildrenViewPresenter(observableText);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+
             observableText.Value = "changed";
             Assert.AreEqual("changed", view.View.viewWithModel.Text.text);
         }
-        
+
         [Test]
-        public void Set_ChangeValue_Unsubscribed()
+        public void Set_Observable_ChangeValue_Unsubscribed()
         {
             var observableText = new Observable<string>("initial");
             var presenter = new ChildrenViewPresenter(observableText);
             UiBootstrap.Instance.Bind(presenter);
             UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
             UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
-            
+
             var view = UiControl.Instance.Create(Locator.viewWithChild);
             observableText.Value = "changed";
-            presenter.unsubscriptionKey.Dispose();
+            presenter.unsubscription.Dispose();
             observableText.Value = "again";
-            
+
             // Unsubscribed, so the value should not change
             Assert.AreEqual("changed", view.View.viewWithModel.Text.text);
         }
-        
+
         [Test]
-        public void Set_ChangeValue_ParentUnset()
+        public void Set_Observable_ChangeValue_ParentUnset()
         {
             var observableText = new Observable<string>("initial");
             var presenter = new ChildrenViewPresenter(observableText);
             UiBootstrap.Instance.Bind(presenter);
             UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
             UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
-            
+
             var view = UiControl.Instance.Create(Locator.viewWithChild);
             observableText.Value = "changed";
             view.Unset();
             observableText.Value = "again";
-            
+
             // Unsubscribed, so the value should not change
             Assert.AreEqual("changed", view.View.viewWithModel.Text.text);
         }
+
+        [Test]
+        public void Set_OptionalObservable_InitialValue_Some_NothingStrategy()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Nothing);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+
+            Assert.AreEqual("initial", view.View.viewWithModel.Text.text);
+            Assert.IsTrue(view.View.viewWithModel.gameObject.activeSelf);
+        }
+
+        [Test]
+        public void Set_OptionalObservable_InitialValue_Some_ActivityStrategy()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+
+            Assert.AreEqual("initial", view.View.viewWithModel.Text.text);
+            Assert.IsTrue(view.View.viewWithModel.gameObject.activeSelf);
+        }
         
+        [Test]
+        public void Set_OptionalObservable_InitialValue_None_NothingStrategy()
+        {
+            var observableText = new OptionalObservable<string>();
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Nothing);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+
+            Assert.IsTrue(view.View.viewWithModel.gameObject.activeSelf);
+        }
+
+        [Test]
+        public void Set_OptionalObservable_InitialValue_None_ActivityStrategy()
+        {
+            var observableText = new OptionalObservable<string>();
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+
+            Assert.IsFalse(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void Set_OptionalObservable_InitialValue_Some_NothingStrategy_ParentUnset()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Nothing);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+            view.Unset();
+            observableText.Value = "changed".Some();
+
+            // Unsubscribed, so the value should not change
+            Assert.AreEqual("initial", view.View.viewWithModel.Text.text);
+            Assert.IsTrue(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        
+        [Test]
+        public void Set_OptionalObservable_InitialValue_Some_Unsubscribed()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Nothing);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+            presenter.unsubscription.Dispose();
+            observableText.Value = "changed".Some();
+
+            // Unsubscribed, so the value should not change
+            Assert.AreEqual("initial", view.View.viewWithModel.Text.text);
+            Assert.IsTrue(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void Set_OptionalObservable_ChangeValue_FromSomeToSome()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+
+            observableText.Value = "changed".Some();
+            Assert.AreEqual("changed", view.View.viewWithModel.Text.text);
+            Assert.IsTrue(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void Set_OptionalObservable_ChangeValue_FromSomeToNone()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+
+            observableText.Value = Option.None<string>();
+            Assert.IsFalse(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void Set_OptionalObservable_ChangeValue_FromNoneToSome()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+
+            observableText.Value = "changed".Some();
+            Assert.AreEqual("changed", view.View.viewWithModel.Text.text);
+            Assert.IsTrue(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void Set_OptionalObservable_ChangeValue_FromNoneToNone()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+
+            observableText.Value = Option.None<string>();
+            Assert.IsFalse(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void Set_OptionalObservable_ChangeValue_FromSomeToSome_Unsubscribed()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+            observableText.Value = "changed".Some();
+            presenter.unsubscription.Dispose();
+            observableText.Value = "again".Some();
+
+            // Unsubscribed, so the value should not change
+            Assert.AreEqual("changed", view.View.viewWithModel.Text.text);
+            Assert.IsTrue(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void Set_OptionalObservable_ChangeValue_FromSomeToNone_Unsubscribed()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+            observableText.Value = Option.None<string>();
+            presenter.unsubscription.Dispose();
+            observableText.Value = "again".Some();
+
+            // Unsubscribed, so the value should not change
+            Assert.AreNotEqual("again", view.View.viewWithModel.Text.text);
+            Assert.IsFalse(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void Set_OptionalObservable_ChangeValue_FromSomeToSome_ParentUnset()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+            observableText.Value = "changed".Some();
+            view.Unset();
+            observableText.Value = "again".Some();
+
+            // Unsubscribed, so the value should not change
+            Assert.AreEqual("changed", view.View.viewWithModel.Text.text);
+            Assert.IsTrue(view.View.viewWithModel.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void Set_OptionalObservable_ChangeValue_FromSomeToNone_ParentUnset()
+        {
+            var observableText = new OptionalObservable<string>("initial");
+            var presenter = new ChildrenViewOptionalPresenter(observableText, ObservableSubscriptionOwnerExtension.OptionalStrategy.Activity);
+            UiBootstrap.Instance.Bind(presenter);
+            UiBootstrap.Instance.Bind<SimpleTextButtonView.Presenter>();
+            UiBootstrap.Instance.Bind<ObservablePresenter<ModelessView>>();
+
+            var view = UiControl.Instance.Create(Locator.viewWithChild);
+            observableText.Value = Option.None<string>();
+            view.Unset();
+            observableText.Value = "again".Some();
+
+            // Unsubscribed, so the value should not change
+            Assert.AreNotEqual("again", view.View.viewWithModel.Text.text);
+            Assert.IsFalse(view.View.viewWithModel.gameObject.activeSelf);
+        }
+
         private class ChildrenViewPresenter : Presenter<ViewWithChild>
         {
             private readonly IReadOnlyObservable<string> _observableText;
-            public IDisposable unsubscriptionKey;
+            public IDisposable unsubscription;
 
             public ChildrenViewPresenter(IReadOnlyObservable<string> observableText)
             {
@@ -128,7 +362,26 @@ namespace Tests.Observables
 
             protected override void OnSet(ViewWithChild view, ISubscriptions subs)
             {
-                unsubscriptionKey = subs.Set(view.viewWithModel, _observableText);
+                unsubscription = subs.Set(view.viewWithModel, _observableText);
+            }
+        }
+
+        private class ChildrenViewOptionalPresenter : Presenter<ViewWithChild>
+        {
+            private readonly IReadOnlyObservable<Option<string>> _observableText;
+            private readonly ObservableSubscriptionOwnerExtension.OptionalStrategy _strategy;
+            public IDisposable unsubscription;
+
+            public ChildrenViewOptionalPresenter(IReadOnlyObservable<Option<string>> observableText,
+                ObservableSubscriptionOwnerExtension.OptionalStrategy strategy)
+            {
+                _observableText = observableText;
+                _strategy = strategy;
+            }
+
+            protected override void OnSet(ViewWithChild view, ISubscriptions subs)
+            {
+                unsubscription = subs.Set(view.viewWithModel, _observableText, _strategy);
             }
         }
     }
--- Yaga/Reactive/SubscriptionOwnerExtensions/ObservableSubscriptionOwnerExtension.cs
+++ Yaga/Reactive/SubscriptionOwnerExtensions/ObservableSubscriptionOwnerExtension.cs
@@ -54,12 +54,78 @@ namespace Yaga.Reactive
                 cancelKey = owner.Add(new Disposable(() => viewControl.Unset()));
             });
 
-            var key =  owner.Add(unsubscription);
+            var key = owner.Add(unsubscription);
             return new Disposable(() =>
             {
                 owner.Remove(key);
                 unsubscription.Dispose();
             });
+        }
+
+        public static IDisposable Set<TView, TModel>(this ISubscriptions owner, TView child,
+            IReadOnlyObservable<Option<TModel>> observableModel, OptionalStrategy strategy = OptionalStrategy.Activity)
+            where TView : MonoBehaviour, IView<TModel>
+        {
+            Option<(ViewControl<TView, TModel> viewControl, ISubscriptions.Key key)> ViewControl(Option<TModel> option)
+            {
+                return option.Match(value =>
+                {
+                    var viewControl = owner.Set(child, value);
+                    var key = owner.Add(new Disposable(() => viewControl.Unset()));
+
+                    switch (strategy)
+                    {
+                        case OptionalStrategy.Activity:
+                            child.gameObject.SetActive(true);
+                            break;
+                        case OptionalStrategy.Nothing:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+
+                    return (viewControl, key).Some();
+                }, () =>
+                {
+                    switch (strategy)
+                    {
+                        case OptionalStrategy.Activity:
+                            child.gameObject.SetActive(false);
+                            break;
+                        case OptionalStrategy.Nothing:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+
+                    return Option.None<(ViewControl<TView, TModel>, ISubscriptions.Key)>();
+                });
+            }
+
+            var viewControl = ViewControl(observableModel.Value);
+            var unsubscription = observableModel.Subscribe(option =>
+            {
+                viewControl.MatchSome(tuple =>
+                {
+                    owner.Remove(tuple.key);
+                    tuple.viewControl.Unset();
+                });
+
+                viewControl = ViewControl(option);
+            });
+
+            var key = owner.Add(unsubscription);
+            return new Disposable(() =>
+            {
+                owner.Remove(key);
+                unsubscription.Dispose();
+            });
+        }
+
+        public enum OptionalStrategy
+        {
+            Activity,
+            Nothing
         }
     }
 }
